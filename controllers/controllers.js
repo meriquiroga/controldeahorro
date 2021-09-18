f342899e22d2b11e154f04d77f58032bfe894d0a
unknown <meriquiroga@hotmail.com>
2021-09-18 12:02:58 -0300

    Corregido error headers sent

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,165 +1,108 @@
-const path = require('path')
-const Movimiento = require('../models/Movimiento')
+const path = require("path");
+const Movimiento = require("../models/Movimiento");
 
 const movimientosControllers = {
-    home: (req, res) => {
-        res.render('index', {
-            title: 'Home',
-            logueado: req.session.logueado, 
-            name: req.session.name,
-            userId: req.session.userId
-        })
-    },
+  home: (req, res) => {
+    res.render("index", {
+      title: "Home",
+      error: null,
+      logueado: req.session.logueado,
+      name: req.session.name,
+      userId: req.session.userId,
+    });
+  },
 
-    balance: async (req, res) => {
-        if (req.session.logueado) {
-            try {
-                const movimientos = await Movimiento.find({userId: req.session.userId})
-                res.render('balance', {
-                title: 'Balance',
-                movimientos,
-                error: null,
-                logueado: req.session.logueado, 
-                name: req.session.name,
-                userId: req.session.userId
-        })
-        } catch(error) {
-            alert(error)
-        }
-    } 
-        res.redirect('/crear-cuenta')
-    },
-    
-    /* balance: async (req, res) => {
-        if (req.session.logueado) {
-            const movimientos = await Movimiento.find()
-            res.render('balance', {
-                title: 'Balance',
-                movimientos,
-                error: null,
-                logueado: req.session.logueado, 
-                name: req.session.name,
-                userId: req.session.userId
-        })
-        }
-        res.redirect('/crear-cuenta')
-    }, */
-    // Ese crear cuenta debería ir a no autorizado y con botón que cree cuenta
-    // Andando antes de sessions
-    /* balance: async (req, res) => {
-        const movimientos = await Movimiento.find()
-        res.render('balance', {
-            title: 'Balance',
-            movimientos,
-            error: null
-        })
-    }, */
-
-    editarPanel: (req, res) => {
-        res.render('editar', {
-            title: 'Editar movimiento',
-            error: null,
-            logueado: req.session.logueado, 
-            name: req.session.name,
-            userId: req.session.userId
-        })
-    },
-
-    guardarMovimiento: async (req, res) => {
-        const {description, number, userId} = req.body
-        let newMovimiento = new Movimiento({
-            description,
-            number, 
-            userId 
-        })
-        console.log(description)
-        console.log(number)
-        try {
-            await newMovimiento.save()
-            res.redirect('/balance')
-        } catch(error) {
-            res.render('editar', {
-                title: 'Editar movimiento',
-                error: error,
-                logueado: req.session.logueado, 
-                name: req.session.name,
-                userId: req.session.userId
-            })
-
-        }
-    }, 
-
-
-    // Este de abajo anda perfecto, lo comento para probar el editar ahí mismo
-    /* guardarMovimiento: async (req, res) => {
-        const {description, number} = req.body
-        let newMovimiento = new Movimiento({
-            description,
-            number
-        })
-        try {
-            await newMovimiento.save()
-            res.redirect('/balance')
-        } catch(error) {
-            res.render('nuevo-movimiento', {
-                title: 'Nuevo movimiento',
-                error: error
-            })
-
-        }
-    },  */
-
-    eliminar: async (req, res) => {
-        await Movimiento.findOneAndDelete({_id: req.params._id})
-        res.redirect('/balance')
-    }, 
-
-    /* editar: async (req, res) => {
-        const {description, number, _id} = req.body
-        let movimiento = await Movimiento.findOne({_id})
-        movimiento.description = description
-        movimiento.number = number
-        try {
-            await movimiento.save()
-            res.redirect('/balance')
-        } catch(error) {
-            res.render('editar', {
-                title: 'Editar movimiento',
-                error: null,
-                editando: false
-            })
-        }
-    } */
-
-    editar: async (req, res) => {
-        let movimiento = await Movimiento.findOne({_id: req.params._id})
-        res.render('editar', {
-            title: 'Editar movimiento',
-            error: null,
-            editando: movimiento,
-            logueado: req.session.logueado, 
-            name: req.session.name,
-            userId: req.session.userId
-        })
+  balance: async (req, res) => {
+    if (req.session.logueado) {
+      try {
+        const movimientos = await Movimiento.find({
+          userId: req.session.userId,
+        });
+        return res.render("balance", {
+          title: "Balance",
+          movimientos,
+          error: null,
+          logueado: req.session.logueado,
+          name: req.session.name,
+          userId: req.session.userId,
+        });
+      } catch (error) {
+        alert(error);
+      }
     }
+    return res.redirect("/crear-cuenta");
+  },
+  /* balance: async (req, res) => {
+    if (req.session.logueado) {
+      try {
+        const movimientos = await Movimiento.find({
+          userId: req.session.userId,
+        });
+        res.render("balance", {
+          title: "Balance",
+          movimientos,
+          error: null,
+          logueado: req.session.logueado,
+          name: req.session.name,
+          userId: req.session.userId,
+        });
+      } catch (error) {
+        alert(error);
+      }
+    }
+    res.redirect("/crear-cuenta");
+  }, */
 
-    /* editar: async (req, res) => {
-        let movimiento = await Movimiento.findOne({_id: req.params._id})
-        res.render('editar', {
-            title: 'Editar movimiento',
-            error: null,
-            editando: movimiento
-        })
-    } */
+  editarPanel: (req, res) => {
+    res.render("editar", {
+      title: "Editar movimiento",
+      error: null,
+      logueado: req.session.logueado,
+      name: req.session.name,
+      userId: req.session.userId,
+    });
+  },
 
-     // nuevoMovimiento es el que se podría eliminar.
-     /* editarMovimiento: (req, res) => {
-        res.render('editarMovimiento', {
-            title: 'Editar movimiento',
-            error: null,
-            editando: false
-        })
-    }, */
-}
+  guardarMovimiento: async (req, res) => {
+    const { description, number, userId } = req.body;
+    let newMovimiento = new Movimiento({
+      description,
+      number,
+      userId,
+    });
+    console.log(description);
+    console.log(number);
+    try {
+      await newMovimiento.save();
+      res.redirect("/balance");
+    } catch (error) {
+      res.render("editar", {
+        title: "Editar movimiento",
+        error: error,
+        logueado: req.session.logueado,
+        name: req.session.name,
+        userId: req.session.userId,
+      });
+    }
+  },
 
-module.exports = movimientosControllers
+  eliminar: async (req, res) => {
+    await Movimiento.findOneAndDelete({ _id: req.params._id });
+    res.redirect("/balance");
+  },
+
+  editar: async (req, res) => {
+    let movimiento = await Movimiento.findOne({ _id: req.params._id });
+    res.render("editar", {
+      title: "Editar movimiento",
+      error: null,
+      editando: movimiento,
+      logueado: req.session.logueado,
+      name: req.session.name,
+      userId: req.session.userId,
+    });
+  },
+
+};
+
+module.exports = movimientosControllers;
--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -32,7 +32,7 @@ const usersControllers = {
         req.session.userId = usuario._id;
         return res.redirect("/balance");
       } else {
-        res.render("ingresar", {
+         return res.render("ingresar", {
           title: "Ingresar",
           error: "E-mail o contraseña incorrectos",
           logueado: req.session.logueado,
@@ -44,27 +44,28 @@ const usersControllers = {
       alert(error);
     }
   },
-
-  // Andando hasta hasheo de password
   /* ingresarForm: async (req, res) => {
-        const {email, password} = req.body
-        let usuario = await Usuario.findOne({email})
-        if (usuario.password === password) {
-            req.session.logueado = true
-            req.session.name = usuario.name
-            req.session.userId = usuario._id 
-            return res.redirect('/balance')
-        }
-        console.log(usuario.name)
-        console.log(usuario._id)
-        res.render('ingresar', {
-            title: 'Ingresar',
-            error: 'E-mail o contraseña incorrectos',
-            logueado: req.session.logueado, 
-            name: req.session.name,
-            userId: req.session.userId
-        })
-    }, */
+    const { email, password } = req.body;
+    try {
+      let usuario = await Usuario.findOne({ email });
+      if (bcryptjs.compareSync(password, usuario.password)) {
+        req.session.logueado = true;
+        req.session.name = usuario.name;
+        req.session.userId = usuario._id;
+        return res.redirect("/balance");
+      } else {
+         res.render("ingresar", {
+          title: "Ingresar",
+          error: "E-mail o contraseña incorrectos",
+          logueado: req.session.logueado,
+          name: req.session.name,
+          userId: req.session.userId,
+        });
+      }
+    } catch (error) {
+      alert(error);
+    }
+  }, */
 
   crearCuentaForm: async (req, res) => {
     const { name, email, password } = req.body;
@@ -83,7 +84,7 @@ const usersControllers = {
         req.session.userId = agregarUser._id;
         return res.redirect("/balance");
       } else {
-        res.render("crear-cuenta", {
+        return res.render("crear-cuenta", {
           title: "Crear cuenta",
           error:
             "E-mail de registro en uso. Por favor, ingresá o creá tu cuenta con uno diferente",
@@ -96,45 +97,6 @@ const usersControllers = {
       alert(error);
     }
   },
-
-  /* addUser: async (req, res) => {
-        const { name, surname, email, password, img, country, google } = req.body;
-        let hashedPass = bcryptjs.hashSync(password);
-        const userInfo = new User({
-          name,
-          surname,
-          email,
-          password: hashedPass,
-          img,
-          country,
-          google,
-        });
-        try {
-          let userExists = await User.findOne({ email: email });
-          if (userExists)
-            throw new Error(
-              "Sign up e-mail already in use, please try again with a different one."
-            );
-          await userInfo.save();
-          const token = jwt.sign({ ...userInfo }, process.env.SECRETORKEY);
-          res.json({
-            success: true,
-            response: { name: userInfo.name, img: userInfo.img, token },
-            error: null,
-          });
-        } catch (error) {
-          res.json({ success: false, response: null, error: error.message });
-        } //de acá sale el error de conexión de back y bd.
-      }, */
-
-  // Anda sin loguear
-  /* crearCuentaForm: async (req, res) => {
-        const {name, email, password} = req.body
-        await new Usuario({name, email, password}).save()
-        req.session.logueado = true
-        req.session.name = name
-        res.redirect('/balance')
-    }, */
 
   salir: (req, res) => {
     req.session.destroy(() => {
