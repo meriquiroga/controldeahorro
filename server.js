7941fa00aacd1afb67776da23da2267a312da38d
unknown <meriquiroga@hotmail.com>
2021-09-19 22:25:18 -0300

    Host

--- server.js
+++ server.js
@@ -24,4 +24,4 @@ app.use(session({
 
 app.use('/', router)
 
-app.listen(4000, () => console.log("Server listening"))
+app.listen(process.env.PORT || 4000, process.env.HOST || '0.0.0.0', () => console.log('Server listening'))
